53458058dd1705872faa9a3c17ea02eb1234bc39
Adomas Auga <35742185+aauga@users.noreply.github.com>
2023-05-22 19:04:23 +0300

    create navbar

--- src/Client/src/App.tsx
+++ src/Client/src/App.tsx
@@ -1,12 +1,14 @@
 import Container from "react-bootstrap/Container";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Main } from "./pages/Main";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Navbar />
       <Container>
         <Main />
       </Container>
